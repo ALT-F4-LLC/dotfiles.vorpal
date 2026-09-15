94298f88a2b0589d0125d9a440787ade0b1dcd21
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2026-09-14 17:47:39 -0700

    fix(claude_code): set proper urls for otel data

--- src/user/claude_code.rs
+++ src/user/claude_code.rs
@@ -14,9 +14,9 @@ const GIT_ALLOWED_SIGNERS_INSTALL_PATH: &str = "${HOME}/.config/git/allowed_sign
 const GIT_AGENT_SIGNING_KEY_PATH: &str = "~/.ssh/agent-signing";
 const GIT_AGENT_SIGNING_KEY_PUBLIC_PATH: &str = "~/.ssh/agent-signing.pub";
 const OTEL_LOGS_ENDPOINT_LOKI: &str =
-    "https://alloy-forwarder.bulbasaur.eks.altf4.internal/v1/metrics";
-const OTEL_METRICS_ENDPOINT_MIMIR: &str =
     "https://alloy-forwarder.bulbasaur.eks.altf4.internal/v1/logs";
+const OTEL_METRICS_ENDPOINT_MIMIR: &str =
+    "https://alloy-forwarder.bulbasaur.eks.altf4.internal/v1/metrics";
 const OTEL_OTLP_PROTOCOL: &str = "http/protobuf";
 const SANDBOX_AGENT_MEMORY_PATH: &str = "~/.claude/agent-memory";
 const SANDBOX_BARE_REPO_ROOT: &str = "~/Development/repository/github.com/ALT-F4-LLC";
